c0b5fb74f8294bcca214ab15687244dec1171a44
jcmoralesdev <127900342+jcmoralesdev@users.noreply.github.com>
2023-07-23 18:36:49 -0600

    unit test

---
+++ tests/Unit/Helpers/BlogTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit\Helpers;
+
+use PHPUnit\Framework\TestCase;
+
+class BlogTest extends TestCase
+{
+    
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
