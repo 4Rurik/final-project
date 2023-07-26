71535055277c1a5f513eb8fc0a45386754944ba8
jcmoralesdev <127900342+jcmoralesdev@users.noreply.github.com>
2023-07-26 12:57:36 -0600

    new test

--- tests/Unit/Helpers/BlogModel.php
+++ tests/Unit/Helpers/BlogModel.php
@@ -4,9 +4,11 @@ namespace Tests\Unit\Helpers;
 
 use PHPUnit\Framework\TestCase;
 
-class UserTest extends TestCase
+class BlogModel extends TestCase
 {
-    
+    /**
+     * A basic unit test example.
+     */
     public function test_example(): void
     {
         $this->assertTrue(true);
