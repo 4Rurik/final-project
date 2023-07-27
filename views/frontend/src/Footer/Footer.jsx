375b95bb314d358c3b0508d668e33626cd44560e
lejazzcat <casey.ceron@gmail.com>
2023-07-27 03:22:59 -0600

    Folders and footer added

---
+++ views/frontend/src/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom";
+const Footer = () => {
+    return(
+        <>
+        <footer className="bg-dark text-white text-center py-3">
+      <Container>
+        <p>&copy; 2023 Blogapp. All rights reserved.</p>
+      </Container>
+    </footer>
+        </>
+    );
+};
+
+export default Footer;
--- views/frontend/src/navbar/Navbar.jsx
+++ views/frontend/src/navbar/Navbar.jsx
@@ -2,13 +2,18 @@ import { NavLink } from "react-router-dom";
 const NavBar = () => {
     return(
         <>
-        <nav className="navbar navbar-dark bg-dark">
-        <div className="container">
+        <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="../" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"></img>
+      Blogapp
+    </a>
             <NavLink to="/" className="btn btn-outline-primary">Home</NavLink>
             <NavLink to="/Blog" className="btn btn-outline-primary">Blogs</NavLink>
-            <NavLink to="/Users" className="btn btn-outline-primary">Users</NavLink>
-        </div>
-        </nav>
+            <NavLink to="/Users" className="btn btn-outline-primary">Usuarios</NavLink>
+            <NavLink to="/Comments" className="btn btn-outline-primary">Comentarios</NavLink>
+  </div>
+</nav>
         </>
     );
 };
